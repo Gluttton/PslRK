783e123b91b205df13cb12e03b8e1b70916a91bb
Gluttton <gluttton@ukr.net>
2013-10-12 17:09:27 +0300

    Implemented CodeManger's Calculator.

--- Tools/CodeManager/Core/src/Calculator.cpp
+++ Tools/CodeManager/Core/src/Calculator.cpp
@@ -2,16 +2,52 @@
 
 
 
-std::vector <int> Calculator::CalculateAcf (const std::string)
+std::vector <int> Calculator::CalculateAcf (const std::string code)
 {
-    const std::vector <int> t {};
-    return t;
+    std::vector <int> acf {};
+
+    for (size_t i = 1; i < code.length (); ++i) {
+        int sum {0};
+        for (size_t j = 0; j < i; ++j) {
+            const int a {code [                     j] == '+' ? 1 : -1};
+            const int b {code [code.length () - 1 - j] == '+' ? 1 : -1};
+            sum += a * b;
+        }
+        acf.push_back (sum);
+    }
+
+    acf.push_back (code.length () );
+
+    for (size_t i = 1; i < code.length (); ++i) {
+        int sum {0};
+        for (size_t j = 0; j < code.length () - i; ++j) {
+            const int a {code [i + j] == '+' ? 1 : -1};
+            const int b {code [    j] == '+' ? 1 : -1};
+            sum += a * b;
+        }
+        acf.push_back (sum);
+    }
+
+
+    return acf;
 }
 
 
 
-int Calculator::CalculateMsl (const std::string)
+int Calculator::CalculateMsl (const std::string code)
 {
-    constexpr int t {0};
-    return t;
+    int msl {0};
+
+    for (size_t i = 1; i < code.length (); ++i) {
+        int sum {0};
+        for (size_t j = 0; j < i; ++j) {
+            const int a {code [                     j] == '+' ? 1 : -1};
+            const int b {code [code.length () - 1 - j] == '+' ? 1 : -1};
+            sum += a * b;
+        }
+        msl = std::max (msl, sum);
+    }
+
+
+    return msl;
 }
