c2eca31391e076b8f90b9990a0ec38d17d013c6e
Gluttton <gluttton@ukr.net>
2013-10-13 15:21:13 +0300

    Calculating of Autocorrelation function was simplified.

--- Tools/CodeManager/Core/src/Calculator.cpp
+++ Tools/CodeManager/Core/src/Calculator.cpp
@@ -1,5 +1,4 @@
 #include "Calculator.h"
-//#include <iostream>
 
 
 
@@ -7,16 +6,20 @@ std::vector <int> Calculator::CalculateAcf (const std::string code)
 {
     std::vector <int> acf {};
 
+    //   |+--|
+    // +-|-  |
+    //  +|-- |
+    //   |+--|
+    //   | +-|-
+    //   |  +|--
     const int range = code.length () - 1;
-    for (int i = range, b = -range, e = 0; i >= -range; --i, ++b, ++e) {
-        const int begin {b >     0 ? b :     0};
-        const int end   {e < range ? e : range};
+    for (int i = range; i >= -range; --i) {
+        const int begin {      - i >     0 ?        -i :     0};
+        const int end   {range - i < range ? range - i : range};
         int sum {0};
-        //std::cout << "i=" << i << ", b=" << b << ", begin=" << begin << ", e=" << e << ", end=" << end << std::endl;
         for (int j = begin; j <= end; ++j) {
             const int a {code [    j] == '+' ? 1 : -1};
             const int b {code [i + j] == '+' ? 1 : -1};
-            //std::cout << "        " << j << " x " << i + j << "    " << a << " x " << b << " = " << a * b << std::endl;
             sum += a * b;
         }
         acf.push_back (sum);
@@ -35,7 +38,6 @@ int Calculator::CalculateMsl (const std::string code)
     for (int i = 0; i < code.length () - 1; ++i) {
         msl = std::max (msl, abs (acf [i]) );
     }
-
 
     return msl;
 }
